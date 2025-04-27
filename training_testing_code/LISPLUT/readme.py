76e2a07ed1d9e7d4b14dd7b04db8453ded9f3c23
zhjy2016 <49719272+zhjy2016@users.noreply.github.com>
2025-04-27 13:29:48 +0800

    Create readme.txt

---
+++ training_testing_code/LISPLUT/readme.py
@@ -0,0 +1,2 @@
+This training is expanded version code and good parameters, can put this part in the https://github.com/zhjy2016/SPLUT/new/main/training_testing_code directory for training.
+                                                                                                                                                               
